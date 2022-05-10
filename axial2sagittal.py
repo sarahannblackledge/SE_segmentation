b6fee30521edac49d163dd59d148760e198a70fe
Sarah Blackledge <sblackledge@25283radlt.icr.ac.uk>
2022-05-10 13:46:08 +0100

    added downsampling and rescaling. Decided to read in mask nrrd files instead of numpy files

--- axial2sagittal.py
+++ axial2sagittal.py
@@ -1,51 +1,78 @@
-#Read in 2D dicom files in axial orientation and convert into directory of 2D sagittal numpy arrays
 import numpy as np
 import SimpleITK as sitk
 import os
 import pydicom as dicom
 import matplotlib.pyplot as plt
+import sys
+sys.path.append('/Users/sblackledge/PycharmProjects/pythonProject/SE_segmentation')
+from downsamplePatient import downsamplePatient
 
+id_nums = [3, 4, 5, 6]
+patient_name = 'Gynae2_0957'
+for j in id_nums:
+    id_num = str(j)
 
-def axial2sagittal(CT_dir, fpath_uterus_mask):
+    character_num = len(id_num)
+
+    if character_num < 2:
+        fpath_mask = "/Users/sblackledge/Documents/Gynae_data_correct/" + patient_name + "/uterus_masks/Uterocervix_0" + id_num + ".nrrd"
+    else:
+        fpath_mask = "/Users/sblackledge/Documents/Gynae_data_correct/" + patient_name + "/uterus_masks/Uterocervix_" + id_num + ".nrrd"
+
+    CT_dir = "/Users/sblackledge/Documents/Gynae_data_correct/" + patient_name + "/CBCT" + id_num + "_resampled"
+
     # Make sitk image object
     files_CT = np.array([os.path.join(CT_dir, fl) for fl in os.listdir(CT_dir) if "dcm" in fl])
     dicoms = np.array([dicom.read_file(fl, stop_before_pixels = True) for fl in files_CT])
     locations = np.array([float(dcm.ImagePositionPatient[-1]) for dcm in dicoms])
     files_CT = files_CT[np.argsort(locations)]
-    CT = sitk.ReadImage(files_CT)
+    CT_sitk = sitk.ReadImage(files_CT)
 
-    #reorient dicom and save as nrrd
-    '''reoriented = sitk.DICOMOrient(CT, 'SPR')
-    #sitk.WriteImage(reoriented, '/Users/sblackledge/Desktop/SPR.nrrd')'''
+    # Load in mask
+    reader = sitk.ImageFileReader()
+    reader.SetImageIO("NrrdImageIO")
+    reader.SetFileName(fpath_mask)
+    uterus_mask = reader.Execute()
 
-    #reorient numpy array
-    CT_im = sitk.GetArrayFromImage(CT)
-    axial_orientation = CT_im.transpose(1, 2, 0)
-    sagittal_orientation = axial_orientation.transpose(2, 0, 1)
-    sagittal_orientation = np.flipud(sagittal_orientation)
+    # Downsample to 256x256
+    CT_sitk = downsamplePatient(CT_sitk, 2, is_label=False)
+    uterus_mask = downsamplePatient(uterus_mask, 2, is_label=True)
 
-    #Load in uterus npy mask
-    ax_label = np.load(fpath_uterus_mask)
-    ax_label = ax_label.transpose(1,0, 2)
-    sag_label = ax_label.transpose(2, 0, 1)
-    sag_label = np.flipud(sag_label)
+    # Extract image arrays from sitk objects
+    orig_im = sitk.GetArrayFromImage(CT_sitk)
+    mask3D = sitk.GetArrayFromImage(uterus_mask)
 
-    plt.figure(1)
-    plt.imshow(axial_orientation[:, :, 76], cmap='gray')
-    plt.contour(ax_label[:, :, 76], 1, colors='m')
+    # Normalize intensity values betwen air(-1024) and bone (~1000)
+    # Note: opted not to normalize between 0 and 1 explicitly because tattoo markers on some patients have HU of > 4000, so would skew data
+    # Chosen approach should approximately normalize between 0 and 1 given that HU of bone can be up to 1000.
+
+    normalized_im3D = (orig_im - np.min(orig_im)) / (1000 - np.min(orig_im))
+
+    # Transpose to conventional slice order
+    normalized_im3D = normalized_im3D.transpose(1, 2, 0)
+    mask3D = mask3D.transpose(1, 2, 0)
+
+    #Transpose to sagittal orientation
+    sag_im3D = normalized_im3D.transpose(2, 0, 1)
+    sag_im3D = np.flipud(sag_im3D)
+
+    sag_mask3D = mask3D.transpose(2, 0, 1)
+    sag_mask3D = np.flipud(sag_mask3D)
+
+    plt.figure()
+    plt.imshow(normalized_im3D[:, :, 125], cmap='gray')
+    plt.colorbar()
+    plt.contour(mask3D[:, :, 125], 1, colors='m')
     plt.show()
 
-    plt.figure(2)
-    plt.imshow(sagittal_orientation[:, :, 250], cmap='gray')
-    plt.contour(sag_label[:, :, 250], 1, colors='m')
+    plt.figure()
+    plt.imshow(sag_im3D[:, :, 125], cmap='gray')
+    plt.colorbar()
+    plt.contour(sag_mask3D[:, :, 125], 1, colors='m')
     plt.show()
 
 
-    return axial_orientation, sagittal_orientation
 
-CT_dir = "/Users/sblackledge/Documents/Gynae_data_correct/RMH008/CBCT2_resampled"
-fpath_uterus_mask = "/Users/sblackledge/Documents/Gynae_data_correct/RMH008/uterus_masks/Uterocervix_02.npy"
-axial_orientation, sagittal_orientation = axial2sagittal(CT_dir, fpath_uterus_mask)
 
 
 
